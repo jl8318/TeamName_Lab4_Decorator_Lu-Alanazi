db672976410b60ecc5b199e795542a2d905a5c34
Jixiang Lu <jl8318@ship.edu>
2016-05-31 15:22:53 -0400

    Using TDD to develop the PlasmaCannon class without attachments
    All tests passed.
    Test and develop include: 
    MaxRange< distance
    MaxRange>distance
    every time the PlasmaCannon is fired it weakens the power it has for the next shot
    ReloadWeapon.

---
+++ src/weapon/PlasmaCannon.java
@@ -0,0 +1,35 @@
+package weapon;
+
+public class PlasmaCannon extends GenericWeapon {
+	
+	public PlasmaCannon()
+	{
+		this(50,20,1,4);
+	}
+	
+	public PlasmaCannon(int damage, int range,int rof, int ammo)
+	{
+		super(damage,range,rof,ammo);
+	}
+
+	@Override
+	public int getDamage(int distance) {
+		int distanceAbs = Math.abs(distance);
+		if(distanceAbs>this.getMaxRange())
+			return 0;
+		else
+		{
+			//get result
+			float baseDamage = (float)this.getBaseDamage();
+			float actualAmmo = (float)this.getActualAmmo();
+			float maxAmmo = (float)this.getMaxAmmo();
+			float damage = baseDamage*(actualAmmo/maxAmmo);
+			int result = (int)damage;
+			//change actualAmmo
+			int actualAmmoInt = ((getActualAmmo()-1)>0)?(getActualAmmo()-1):0;
+			this.setActualAmmo(actualAmmoInt);
+			return result;
+		}
+	}
+
+}
---
+++ test/weapon/TestPlasmaCannon.java
@@ -0,0 +1,56 @@
+package weapon;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPlasmaCannon {
+
+	@Test
+	public void testInitialization() {
+		//test the construct without any parameters
+		Weapon pc = new PlasmaCannon();
+		assertTrue(pc instanceof Weapon);
+		assertTrue(pc instanceof GenericWeapon);
+		assertEquals(4,pc.getMaxAmmo());
+		assertEquals(4,pc.getActualAmmo());
+		assertEquals(50,pc.getBaseDamage());
+		assertEquals(1,pc.getRateOfFire());
+		assertEquals(1,pc.getActualRateFire());
+		assertEquals(20,pc.getMaxRange());
+		
+		//test the construct with four parameters;
+		pc = new PlasmaCannon(50,20,1,4);
+		assertEquals(4,pc.getMaxAmmo());
+		assertEquals(4,pc.getActualAmmo());
+		assertEquals(50,pc.getBaseDamage());
+		assertEquals(1,pc.getRateOfFire());
+		assertEquals(1,pc.getActualRateFire());
+		assertEquals(20,pc.getMaxRange());
+		
+	}
+	
+	@Test
+	public void testGetDamage()
+	{
+		Weapon pc = new PlasmaCannon();
+		// distance < maxRange
+		int expect = 50*(4/4);
+		assertEquals(expect,pc.getDamage(10));
+		expect = 50*3/4;
+		assertEquals(expect,pc.getDamage(10));
+		expect = 50*2/4;
+		assertEquals(expect,pc.getDamage(10));
+		expect =  50*1/4;
+		assertEquals(expect,pc.getDamage(10));
+		//The actualAmmo == 0;
+		assertEquals(0,pc.getDamage(10));
+		//reload
+		pc.reloadWeapon();
+		expect = 50*(4/4);
+		assertEquals(expect,pc.getDamage(10));
+		//when the distance > MaxRange
+		assertEquals(0,pc.getDamage(30));
+	}
+
+}
